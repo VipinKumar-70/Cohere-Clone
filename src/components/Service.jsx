4e369d9e424cbd9e40490470b86c60fcc80e92c9
vipinKumar-70 <vk20140074250@gmail.com>
2024-11-21 00:46:41 +0530

    initial file add

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,76 +1,127 @@
 import React from "react";
 import banner from "../assets/banner.png";
 import model from "../assets/model.png";
+import growth from "../assets/growth.png";
 import Button from "./Button";
-import { RxFontSize } from "react-icons/rx";
 
 function Service() {
   return (
-    <section className="relative w-full bg-bottom overflow-hidden py-4">
-      {/* Background Image */}
-      <div className="absolute inset-0 h-full w-full">
-        <img
-          src={banner}
-          alt="Background showcasing our services"
-          className="h-full w-full object-cover"
-        />
-      </div>
-
-      <div className="2xl:px-40 lg:px-22 px-6">
-        {/* First Section */}
-        <div className="relative flex items-center justify-between flex-wrap md:flex-nowrap w-full py-12 font-starcil font-semibold">
-          <div className="sm:w-full md:w-3/4 xl:w-[50%] lg:w-[70%]">
-            <div className="flex gap-3 mb-3">
-              <p>OUR MODELS</p>
-              <img
-                src="https://cdn.sanity.io/images/rjtqmwfu/production/4ef2d29439c7ab799af033f583a7f04543f91ae8-16x16.svg"
-                alt=""
-              />
-            </div>
-            <div>
-              <p className="lg:text-4xl md:text-3xl text-2xl font-semibold">
-                Our models are designed to augment and elevate the global
-                workforce, so businesses can thrive and stay competitive in the
-                AI era.
-              </p>
-            </div>
+    <div
+      className="2xl:px-40 lg:px-22 px-6 bg-cover bg-center flex flex-col gap-y-6 py-10"
+      style={{ backgroundImage: `url(${banner})` }}
+    >
+      {/* First Section */}
+      <div className="relative flex items-center justify-between flex-wrap md:flex-nowrap w-full py-12 font-starcil font-semibold">
+        <div className="sm:w-full md:w-3/4 xl:w-[50%] lg:w-[70%]">
+          <div className="flex gap-3 mb-3">
+            <p>OUR MODELS</p>
+            <img
+              src="https://cdn.sanity.io/images/rjtqmwfu/production/4ef2d29439c7ab799af033f583a7f04543f91ae8-16x16.svg"
+              alt=""
+            />
           </div>
-
-          <div className="w-full md:w-1/2 flex justify-center md:justify-end">
-            <img src={model} alt="" className="max-w-full h-auto" />
+          <div>
+            <p className="lg:text-4xl md:text-3xl text-2xl font-semibold">
+              Our models are designed to augment and elevate the global
+              workforce, so businesses can thrive and stay competitive in the AI
+              era.
+            </p>
           </div>
         </div>
 
-        {/* Second Section */}
-        <div className="relative flex items-center justify-between flex-wrap md:flex-nowrap w-full py-12 font-starcil font-semibold bg-[#ffffff96] rounded-3xl">
-          <div className="w-full md:w-1/2 flex justify-center md:justify-end md:order-1 order-2">
-            <img src={model} alt="" className="max-w-full h-auto" />
-          </div>
-          <div className="sm:w-full md:w-3/4 lg:w-[60%] xl:w-[50%] flex justify-start flex-wrap flex-col 2xl:px-20 lg:px-10 px-4 order-1 md:order-2">
-            <div className="flex gap-3 items-center text-xs my-2">
-              <p className="p-2 bg-purple-400 rounded-lg">NEW</p>
-              <p>COMMAND</p>
-              <img
-                src="https://cdn.sanity.io/images/rjtqmwfu/production/a1909385aa103bc3e54a2d23908601d7efbab49f-16x16.svg"
-                alt=""
-                className="w-4 h-4"
-              />
-            </div>
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+          <img src={model} alt="" className="max-w-full h-auto" />
+        </div>
+      </div>
 
-            <div>
-              <p className="lg:text-4xl md:text-3xl text-2xl font-medium mb-8">
-                Cohere Command, powering innovation with enterprise GenAI
-              </p>
-              <p className="mb-10">
-                Command models are used by companies to build production-ready,
-                scalable and efficient AI-powered applications.
-              </p>
-              <Button title="Learn more" />
-            </div>
+      {/* Second Section */}
+      <div className="relative flex items-center justify-between flex-wrap md:flex-nowrap w-full py-12 font-starcil font-semibold bg-[#ffffff96] rounded-3xl">
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end md:order-1 order-2">
+          <img src={growth} alt="" className="max-w-full h-auto" />
+        </div>
+        <div className="sm:w-full md:w-3/4 lg:w-[60%] xl:w-[50%] flex justify-start flex-wrap flex-col 2xl:px-20 lg:px-10 px-4 order-1 md:order-2">
+          <div className="flex gap-3 items-center text-xs my-2">
+            <p className="p-2 bg-purple-400 rounded-lg">NEW</p>
+            <p>COMMAND</p>
+            <img
+              src="https://cdn.sanity.io/images/rjtqmwfu/production/a1909385aa103bc3e54a2d23908601d7efbab49f-16x16.svg"
+              alt=""
+              className="w-4 h-4"
+            />
+          </div>
+
+          <div>
+            <p className="lg:text-4xl md:text-3xl text-2xl font-medium mb-8">
+              Cohere Command, powering innovation with enterprise GenAI
+            </p>
+            <p className="mb-10 font-medium">
+              Command models are used by companies to build production-ready,
+              scalable and efficient AI-powered applications.
+            </p>
+            <Button title="Learn more" />
           </div>
         </div>
       </div>
-    </section>
+
+      {/* Third Section */}
+      <div className="relative flex items-center justify-between flex-wrap md:flex-nowrap w-full py-12 font-starcil font-semibold bg-[#ffffff96] rounded-3xl">
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end order-2">
+          <img src={model} alt="" className="max-w-full h-auto" />
+        </div>
+        <div className="sm:w-full md:w-3/4 lg:w-[60%] xl:w-[50%] flex justify-start flex-wrap flex-col 2xl:px-20 lg:px-10 px-4 order-1">
+          <div className="flex gap-3 items-center text-xs my-2">
+            <p>Embed</p>
+            <img
+              src="https://cdn.sanity.io/images/rjtqmwfu/production/edc039dd5d2fa738f669b38dba03cd6de786a7ef-16x16.svg"
+              alt=""
+              className="w-4 h-4"
+            />
+          </div>
+
+          <div>
+            <p className="lg:text-4xl md:text-3xl text-2xl font-medium mb-8">
+              Cohere Embed, unlocking the full potential of your enterprise data
+            </p>
+            <p className="mb-10 font-medium">
+              We have trained our models on the language of business to enable
+              the most accurate and efficient solution. Scale your Enterprise AI
+              strategy starting with the highest performing embedding model,
+              which supports over 100 languages.
+            </p>
+            <Button title="Learn more" />
+          </div>
+        </div>
+      </div>
+
+      {/* Forth Section */}
+      <div className="relative flex items-center justify-between flex-wrap md:flex-nowrap w-full py-12 font-starcil font-semibold bg-[#ffffff96] rounded-3xl">
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+          <img src={growth} alt="" className="max-w-full h-auto" />
+        </div>
+        <div className="sm:w-full md:w-3/4 lg:w-[60%] xl:w-[50%] flex justify-start flex-wrap flex-col 2xl:px-20 lg:px-10 px-4">
+          <div className="flex gap-3 items-center text-xs my-2">
+            <p>RETRIEVAL & RERANK</p>
+            <img
+              src="https://cdn.sanity.io/images/rjtqmwfu/production/3a6986e934884dcfd228cf2bdd4680c1915d8d43-16x16.svg"
+              alt=""
+              className="w-4 h-4"
+            />
+          </div>
+
+          <div>
+            <p className="lg:text-4xl md:text-3xl text-2xl font-medium mb-8">
+              Cohere Rerank, surfacing the industry’s most accurate responses.
+            </p>
+            <p className="mb-10 font-medium">
+              The combination of Rerank and Embed delivers the most reliable and
+              up-to-date responses for your application, grounding
+              retrieval-augmented generation (RAG) in your data.
+            </p>
+            <Button title="Learn more" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
